cbec0f268a66578783c2592440c634cf71536986
Aadith S <62766172+ProxyHydra@users.noreply.github.com>
2021-09-04 15:46:32 +0530

    Preview Edit

--- Judgement Card Game Scoreboard/Scoreboard in main.cpp
+++ Judgement Card Game Scoreboard/Scoreboard in main.cpp
@@ -12,5 +12,19 @@ void main()
 	cout << "Welcome to Judgement Game\n\nEnter the number of players:";
 	cin >> totalplayers;
 	vector<string> playernames(totalplayers);
+	cout << "\nEnter names of players:\n";
+	
+	for (int i = 0; i < totalplayers; i++)
+	{
+		cout << "Player " << i + 1 << ", enter you name: ";
+		cin >> playernames[i];
+	}
+
+	cout << "\nPlayer list:\n";
+	for (int i = 0; i < totalplayers; i++)
+	{
+		cout << "Player " << i + 1 << ": " << playernames[i] << endl;
+	}
+	
 	cout << "\n\nsize= " << sizeof(playernames)/sizeof(playernames[0]);
 }
